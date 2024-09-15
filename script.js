5f0dec9bebd019605321daf5bd23fe28c77a5a53
https://github.com/mazharktk5 <mazharahmad862@gmail.com>
2024-09-15 00:33:59 -0700

    updated

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
     
         function changeMode() {
             var button = document.getElementById("btn");
+            var button1 = document.getElementById("btn1");
             var body = document.querySelector("body");
 
             if (body.style.backgroundColor === "black") {
@@ -9,15 +10,21 @@
                 body.style.backgroundColor = "white";
                 body.style.color = "black";
                 button.style.backgroundColor = "black";
+                button1.style.backgroundColor = "black";
                 button.style.color = "white";
+                button1.style.color = "white";
                 button.textContent = "night";
+                button1.textContent = "Light Mode";
             } else {
         
                 body.style.backgroundColor = "black";
                 body.style.color = "white";
                 button.style.backgroundColor = "white";
+                button1.style.backgroundColor = "white";
                 button.style.color = "black";
+                button1.style.color = "black";
                 button.textContent = "day";
+                button1.textContent = "Dark Mode";
             }
         }
    
